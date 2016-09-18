dc5d304b6ffbf3b4ac5c76294ca80390cb44c7ff
irekrog <irekfrontend@gmail.com>
2016-09-18 20:30:04 +0200

    update dependencies

--- src/view/MapFestivalView.js
+++ src/view/MapFestivalView.js
@@ -31,19 +31,19 @@ export default class MapFestivalView extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    position: 'relative',
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
     justifyContent: 'flex-end',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   map: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0,
+    bottom: 0
   },
 });
